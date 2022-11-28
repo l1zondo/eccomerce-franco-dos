328f4074b24b25a6047f4ce2952b23169ccd925c
Franco Lizondo <madlizondo@gmail.com>
2022-11-27 22:44:13 -0300

    arreglo firebase

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCN6nPxKc1rpluBKO4SxS9Ilz1K5djd7tc",
-  authDomain: "react-coder-b0306.firebaseapp.com",
-  projectId: "react-coder-b0306",
-  storageBucket: "react-coder-b0306.appspot.com",
-  messagingSenderId: "269885246811",
-  appId: "1:269885246811:web:cedba3fb85fa0bd73d1e7e",
-  measurementId: "G-KMCQVWVQ15"
+  apiKey: "AIzaSyDYOMfRewIJ5VaEte73Pm7YocKU1RfKut4",
+  authDomain: "ecommerce-franco-dos.firebaseapp.com",
+  projectId: "ecommerce-franco-dos",
+  storageBucket: "ecommerce-franco-dos.appspot.com",
+  messagingSenderId: "1013382500113",
+  appId: "1:1013382500113:web:fac97c865d6447270846b0"
 };
 
 // Initialize Firebase
